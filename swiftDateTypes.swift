ff5ba031ca37956690f28687d05ea0228fdaa786
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-30 03:34:26 +0530

    data types
    Swift program to explain all the data types in the language.

---
+++ swiftDateTypes.swift
@@ -0,0 +1,30 @@
+//Data types in Swift programming
+//this is a Int data type
+var num: Int
+num = 10
+print(num)
+
+//this is a floating point data type
+var numFloat: Float
+numFloat = 4.5
+print(numFloat)
+
+//this is a double data type
+var numDouble: Double
+numDouble = 4.5555
+print(numDouble)
+
+//this is a character data type
+var charType: Character
+charType = "C";
+print(charType)
+
+//this is a string data type
+var stringType: String
+stringType = "hello, world!"
+print(stringType)
+
+//this is a boolean data type
+var boolType: Bool
+boolType = true;
+print(boolType)
