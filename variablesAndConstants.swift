3820262fa44137c211d862ecdfb589f907b220e5
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-30 03:29:32 +0530

    variables and constants
    swift program to explain all the variables and constants in the language.

---
+++ variablesAndConstants.swift
@@ -0,0 +1,46 @@
+//variables and constants in swift programming.
+//swift variables
+var num = 10;
+//in swift the keyword 'var' is used to declare a variable.
+
+var siteName: String
+//by doing this, the variable 'siteName' can now only store strings.
+
+//assigning values to variables
+var sentence: String
+sentence = "Swift is cool!"
+print(sentence)
+
+//changing the value of a variable
+sentence = "python is great!!!"
+print(sentence)
+
+//declaring constants in swift
+//you can declare a constant variable in swift by using the 'let' keyword for declaration
+let stringSentence = "All programming languages is great!!!"
+print(stringSentence)
+//trying to change the constant variable would cause an error.
+/*stringSentence = "what??"
+print(stringSentence)*/
+
+//swift literals
+//integers literals
+var number: Int
+number = 10
+print(number)
+//floating point literals
+var numberFloat: Float
+numberFloat = 4.5
+print(numberFloat)
+//boolean literals
+var value: Bool
+value = true
+print(value)
+//character literals
+var oneVowel: Character
+oneVowel = "a"
+print(oneVowel)
+//String literals
+var oneString: String
+oneString = "hello, world!"
+print(oneString)
