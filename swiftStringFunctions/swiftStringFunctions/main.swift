7ef1efc7d2017e418bfe52ca307294a811dd9190
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-30 18:32:57 +0530

    swift string functions
    inbuilt string functions in swift programming.

---
+++ swiftStringFunctions/swiftStringFunctions/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  swiftStringFunctions
+//
+//  Created by Vedant Mistry on 30/08/21.
+//
+
+import Foundation
+
+//uppercased function
+var string = "hello, world!"
+var uppercaseString = string.uppercased()
+print(uppercaseString)
+
+//isEmpty function
+var isEmptyFunction = string.isEmpty;
+print(isEmptyFunction) //returns false, if the string is not empty, and returns true if the string is empty.
+
+//lowercased function
+var newString = "HELLO WORLD"
+var lowercaseString = newString.lowercased()
+print(lowercaseString)
+
+
