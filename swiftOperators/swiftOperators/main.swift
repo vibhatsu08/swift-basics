38980547bcfa2f0d717559422125e511b5547369
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-08-30 19:50:30 +0530

    basic operators in swift programming

---
+++ swiftOperators/swiftOperators/main.swift
@@ -0,0 +1,44 @@
+//
+//  main.swift
+//  swiftOperators
+//
+//  Created by Vedant Mistry on 30/08/21.
+//
+
+import Foundation
+
+//operators in swift programming
+// + operator
+var num1: Int = 10
+var num2: Int = 20
+
+//arithmetic operators
+print(num1 + num2) // + operator
+print(num1 - num2) // - operator
+print(num1 / num2) // / operator
+print(num1 * num2) // * operator
+print(num1 % num2) // % operator
+
+//assignment operators
+num1 += 10
+print(num1)
+num1 -= 10
+print(num1)
+num1 /= 10
+print(num1)
+num1 *= 10
+print(num1)
+num1 %= 10
+print(num1)
+
+//comparison operators
+var number1: Int = 100
+var number2: Int = 200
+print(number1 == number2)
+print(number1 > number2)
+print(number1 < number2)
+print(number1 >= number2)
+print(number1 <= number2)
+
+//logical operators
+print()
