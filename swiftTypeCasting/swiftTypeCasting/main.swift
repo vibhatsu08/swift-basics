ae02ee3e1b98809209d481fdf3cd04397c96d335
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-09-18 20:39:30 +0530

    Type Casting in swift programming

---
+++ swiftTypeCasting/swiftTypeCasting/main.swift
@@ -0,0 +1,39 @@
+//
+//  main.swift
+//  swiftTypeCasting
+//
+//  Created by Vedant Mistry on 18/09/21.
+//
+
+import Foundation
+
+/*//type casting in swift programming
+
+func getClientPet() -> Animal{
+    //returns the pet
+}
+let pet = getClientPet() //pet is the type of animal
+//functions for the action to be performed
+func walk(dog: Dog){ //for dog animal
+    print("Walking \(dog.name)")
+}
+func cleanLitterBox(cat: Cat){ //for cat animal
+    print("Cleaning the cat's \(cat.boxSize) litter box.")
+}
+func cleanCage(bird: Bird){ //for bird animal
+    print("removing the \(bird.featherColor) feathers of the bird.")
+}
+let pets = allClientAnimals() //pets refers to all the animals mentioned above
+for pet in pets{ //for example: cat in dog, cat, bird
+    if let dog = pet as? Dog{ //if dog is the specified animal
+        walk(dog: Dog)
+    }
+    else if let cat = pet as? Cat{ //if cat is the specified animal
+        cleanLitterBox(cat: Cat)
+    }
+    else if let bird = pet as? Bird{ //if bird is the specified animal
+        cleanCage(bird: Bird)
+    }
+}
+//us the 'as!' operator when the return type is specific*/
+
